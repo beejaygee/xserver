dcf9ecb337ab140d8690df45bc0547908f91558d
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-02 17:51:56 +0200

    Xext: dpms: use REPLY_*() macros for preparing / sending replies
    Use the new macros for preparing and sending replies to clients.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/dpms.c
+++ Xext/dpms.c
@@ -315,10 +315,8 @@ ProcDPMSGetVersion(ClientPtr client)
         .minorVersion = SERVER_DPMS_MINOR_VERSION
     };
 
-    if (client->swapped) {
-        swaps(&reply.majorVersion);
-        swaps(&reply.minorVersion);
-    }
+    REPLY_FIELD_CARD16(majorVersion);
+    REPLY_FIELD_CARD16(minorVersion);
 
     return X_SEND_REPLY_SIMPLE(client, reply);
 }
@@ -331,10 +329,6 @@ ProcDPMSCapable(ClientPtr client)
     xDPMSCapableReply reply = {
         .capable = TRUE
     };
-
-    if (client->swapped) {
-        swaps(&rep.sequenceNumber);
-    }
 
     return X_SEND_REPLY_SIMPLE(client, reply);
 }
@@ -350,11 +344,9 @@ ProcDPMSGetTimeouts(ClientPtr client)
         .off = DPMSOffTime / MILLI_PER_SECOND
     };
 
-    if (client->swapped) {
-        swaps(&reply.standby);
-        swaps(&reply.suspend);
-        swaps(&reply.off);
-    }
+    REPLY_FIELD_CARD16(standby);
+    REPLY_FIELD_CARD16(suspend);
+    REPLY_FIELD_CARD16(off);
 
     return X_SEND_REPLY_SIMPLE(client, reply);
 }
@@ -453,9 +445,7 @@ ProcDPMSInfo(ClientPtr client)
         .state = DPMSEnabled
     };
 
-    if (client->swapped) {
-        swaps(&reply.power_level);
-    }
+    REPLY_FIELD_CARD16(power_level);
     return X_SEND_REPLY_SIMPLE(client, reply);
 }
 
