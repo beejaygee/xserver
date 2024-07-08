6dc8b7ebbf4a9abe34805e1064a05aaae65228e9
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-08 17:57:03 +0200

    Xext: xtest: use REPLY_*() macros for preparing / sending replies
    Use the new macros for preparing and sending replies to clients.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xtest.c
+++ Xext/xtest.c
@@ -99,8 +99,7 @@ ProcXTestGetVersion(ClientPtr client)
         .minorVersion = XTestMinorVersion
     };
 
-    if (client->swapped)
-        swaps(&reply.minorVersion);
+    REPLY_FIELD_CARD16(minorVersion);
 
     return X_SEND_REPLY_SIMPLE(client, reply);
 }
