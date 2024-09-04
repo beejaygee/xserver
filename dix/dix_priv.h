03fe72d4b8841950eef78a7d7d8081629a331fe1
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-04 18:42:15 +0200

    dix: lookup function for WindowPtr by XID
    This new lookup function retrieves a pointer to WindowRec structure by
    associated XID. Unlike dixLookupWindow(), this one works globally, instead
    of just on one specific client, and it doesn't do any XACE calls.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/dix_priv.h
+++ dix/dix_priv.h
@@ -745,4 +745,15 @@ static inline int xmitClientEvent(ClientPtr pClient, xEvent ev)
     return WriteToClient(pClient, sizeof(xEvent), &ev);
 }
 
+/*
+ * @brief lookup window by XID
+ *
+ * This globally looks for Window with given XID (all screens, all clients)
+ * and returns a pointer to it. If not found, returns NULL.
+ *
+ * Unlike ::dixLookupWindow() it doesn't scan only one given client, nor does
+ * it do any XACE calls.
+ */
+WindowPtr dixLookupWindowByXID(Window window);
+
 #endif /* _XSERVER_DIX_PRIV_H */
