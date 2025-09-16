b64fb5cd73f939d9b53402e5c59d2187420dbce0
Enrico Weigelt, metux IT consult <info@metux.net>
2025-09-16 13:14:39 +0200

    dix: use xorg_list saveSet list
    Simplify saveSet handling by just using trivial `struct xorg_list` instead
    of complicated dynamically resized array. Common operations like insert or
    remove are much simpler using lists instead of arrays.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/dispatch.c
+++ dix/dispatch.c
@@ -3648,6 +3648,7 @@ InitClient(ClientPtr client, int i, void *ospriv)
     client->index = i;
     xorg_list_init(&client->ready);
     xorg_list_init(&client->output_pending);
+    xorg_list_init(&client->saveSets);
     client->clientAsMask = ((Mask) i) << CLIENTOFFSET;
     client->closeDownMode = i ? DestroyAll : RetainPermanent;
     client->requestVector = InitialVector;
